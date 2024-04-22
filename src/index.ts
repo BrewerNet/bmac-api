4eb50b5687aa412219a3691a35cb882b39886aa7
V1ncentNg <112238267+V1ncentNg@users.noreply.github.com>
2024-04-22 16:26:11 +1000

    Init Swagger UI

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from "dotenv";
-import userRouter from './routers';
+// import userRouter from './routers';
 import { Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from "cors";
 import { AllExceptionMiddleware } from './middlewares/AllExceptionMiddleware';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './swagger.json';
+
 
 dotenv.config()
 const app=express()
@@ -12,11 +15,12 @@ const port=process.env.PORT || 8080
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/health',(req:Request,res:Response)=>{
     res.send("health check success")
 })
 
-app.use('/api/v1',userRouter)
+// app.use('/api/v1',userRouter)
 
 app.use(AllExceptionMiddleware);
 
