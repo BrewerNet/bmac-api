29ad30926bf87b2a9a80dbf9443f75fdcc28b7b5
V1ncentNg <112238267+V1ncentNg@users.noreply.github.com>
2024-04-30 16:09:20 +1000

    Implemented API Gateway

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,71 @@ import morgan from "morgan";
 import cors from "cors";
 import { AllExceptionMiddleware } from "./middlewares/AllExceptionMiddleware";
 import swaggerUi from "swagger-ui-express";
-import * as swaggerDocument from "./swagger.json";
+import * as swaggerDocument from "./swagger.json"
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
+
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
+//Limit the access rate of request
+const rateLimit = 50;
+const interval = 60 * 1000;
+const reqCounts = {}
+const blacklist = []
+const whitelist = []
+
+const checkBlackList = (req,res,next) => {
+  if (blacklist.includes(req.ip)){
+    return res.status(400).json({
+      code: 400,
+      status: "Error",
+      message: "Bad Request.",
+    })
+  }
+
+  next();
+}
+
+// Reset interval for every IP address
+setInterval(() => {
+  Object.keys(reqCounts).forEach((ip) => {
+    if (reqCounts[ip] > 0) {
+      reqCounts[ip] = 0;
+    }
+  });
+}, interval);
+
+const rateLimitAndTimeout = (req,res,next) => {
+  const ip = req.ip;
+  reqCounts[ip] = (reqCounts[ip] || 0) + 1;
+
+  if (reqCounts[ip] > rateLimit) {
+    // Respond with a 429 Too Many Requests status code
+    if(!whitelist.includes(ip)){
+      blacklist.push(ip);
+    }
+    return res.status(429).json({
+      code: 429,
+      status: "Error",
+      message: "Rate limit exceeded.",
+      data: null,
+    });
+  }
+  next();
+}
+
+app.use(checkBlackList);
+app.use(rateLimitAndTimeout);
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get("/health", (req: Request, res: Response) => {
   res.send("health check success");
 });
-
 app.use("/api/v1", userRouter);
-
 app.use(AllExceptionMiddleware);
 
 app.listen(port, () => {
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,10 +1,37 @@
 import express from "express";
 import authRouter from "./AuthRouter";
 import userRouter from "./UserRouter";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.use("/auth", authRouter);
-router.use("/user", userRouter);
+//Test JWT 
+const jwtMiddleware = (req, res, next) => {
+    
+  const token = req.headers.authorization;
+
+  if(!token){
+      return res.status(401).json({ message: 'Authorization header missing' });
+  }
+  
+  try{
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  }catch(err){
+      return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+
+//Access control -> Test the JWT based on the route
+router.use("/user", jwtMiddleware, userRouter);
+router.use("/auth", (req,res,next) => {
+  if(req.path === '/login' || req.path === '/sign-up'){
+    next();
+  }
+  else{
+    jwtMiddleware(req,res,next);
+    next();
+  }
+}, authRouter);
 
 export default router;
